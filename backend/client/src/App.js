91df7907c9661a5359a868acfb2f4aea905affb7
Omm Prakash Sahoo <antoniocolapso7@gmail.com>
2023-07-27 17:50:35 +0530

    Improved darkmode n UI

--- backend/client/src/App.js
+++ backend/client/src/App.js
@@ -24,36 +24,34 @@ function App() {
   };
 
   const handleDarkModeToggle = () => {
-    setDarkMode(!darkMode);
+    setDarkMode(prevDarkMode => !prevDarkMode);
   };
 
   return (
-    <>
-      <div className={`container ${darkMode ? 'dark-mode' : ''}`}>
-        <h1>Antonio Colpaso's Compiler</h1>
-        <select
-          value={language}
-          onChange={(e) => setLanguage(e.target.value)}
-          className="language-select"
-        >
-          <option value="py">Python</option>
-          <option value="cpp">C++</option>
-          <option value="rb">Ruby</option>
-        </select>
-        <textarea
-          rows="20"
-          cols="75"
-          className={`textarea ${darkMode ? 'dark-mode' : ''}`}
-          value={code}
-          onChange={(e) => setCode(e.target.value)}
-        ></textarea>
-        <button onClick={handleSubmit}>Submit</button>
-        <p className={darkMode ? 'dark-mode' : ''}>{output}</p>
-        <button onClick={handleDarkModeToggle}>
-          Toggle Dark Mode
-        </button>
-      </div>
-    </>
+    <div className={`container ${darkMode ? 'dark-mode' : ''}`}>
+      <h1>Antonio Colpaso's Compiler</h1>
+      <select
+        value={language}
+        onChange={(e) => setLanguage(e.target.value)}
+        className="language-select"
+      >
+        <option value="py">Python</option>
+        <option value="cpp">C++</option>
+        <option value="rb">Ruby</option>
+      </select>
+      <textarea
+        rows="20"
+        cols="75"
+        className={`textarea ${darkMode ? 'dark-mode' : ''}`}
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      ></textarea>
+      <button onClick={handleSubmit}>Submit</button>
+      <p className={darkMode ? 'dark-mode' : ''}>{output}</p>
+      <button onClick={handleDarkModeToggle}>
+        {darkMode ? 'Light Mode' : 'Dark Mode'}
+      </button>
+    </div>
   );
 }
 
